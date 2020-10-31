c5a817dec7781aa4c43ad0dbb45d3ea0457b62e2
younes <yb74@hotmail.fr>
2020-10-31 23:06:04 +0100

    Fixing the Error when returning BurgerIngredient component by setting the key to igKey+i instead of igKey+1.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,7 +7,7 @@ const burger = (props) => {
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
-                return <BurgerIngredient key={igKey +1} type={igKey}/>
+                return <BurgerIngredient key={igKey +i} type={igKey}/>
             });
         })
         .reduce((arr, el) => {
