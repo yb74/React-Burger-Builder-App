1f5345712a771eb8f5c3e4584a3aabc932111f64
yb74 <yb74@hotmail.fr>
2020-11-01 19:14:48 +0100

    Adding Navigation Items and a Sidedrawer (burger menu)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
-import Toolbar from '../../components/Navigation/Toolbar';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Auxiliary>
         <Toolbar/>
+        <SideDrawer/>
         <main className={classes.Content}>
             {props.children}
         </main>
