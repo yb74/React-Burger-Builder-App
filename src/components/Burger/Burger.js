c58d0944885a86ed19be8ffcf306bab9b223640a
yb74 <yb74@hotmail.fr>
2020-11-01 11:41:04 +0100

    - Fixing the Error when returning BurgerIngredient component by setting the key to igKey+i instead of igKey+1.
    - Adding forgotten semicolons

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,7 +7,7 @@ const burger = (props) => {
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
-                return <BurgerIngredient key={igKey +1} type={igKey}/>
+                return <BurgerIngredient key={igKey +i} type={igKey}/>;
             });
         })
         .reduce((arr, el) => {
@@ -23,6 +23,6 @@ const burger = (props) => {
            <BurgerIngredient type="bread-bottom"/>
        </div>
     );
-}
+};
 
 export default burger;
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -41,6 +41,6 @@ class BurgerIngredient extends Component {
 
 BurgerIngredient.propTypes = {
     type: PropTypes.string.isRequired
-}
+};
 
 export default BurgerIngredient;
