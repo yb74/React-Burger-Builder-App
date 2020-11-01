e39084cb6a24fe04e7473efbb28667a865daebb6
yb74 <yb74@hotmail.fr>
2020-11-01 18:04:14 +0100

    Navigation: Implementing the toolbar and the logo.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
+import Toolbar from '../../components/Navigation/Toolbar';
 
 const layout = (props) => (
     <Auxiliary>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar/>
         <main className={classes.Content}>
             {props.children}
         </main>
