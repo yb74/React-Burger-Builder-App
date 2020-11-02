52e8a9ecb42a7c4c4e6041801bf2db87b8909bee
younes <yb74@hotmail.fr>
2020-11-02 21:10:13 +0100

    Adding some lifeCycles to the app to improve its performance. (don't rerender <OrderSummary> component if we don't need that by using a shouldComponentUpdate on the <Modal> component that wrap <OrderSummary>.)

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Auxiliary from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummaru = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (
-                <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                </li>
-            )
-        });
-    return (
-        <Auxiliary>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Auxiliary>
-    );
-};
+class OrderSummaru extends Component {
+    // This could be a functional component, doesn't have to be a class
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[OrderSummary] DidUpdate');
+    }
 
-export default orderSummaru;
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                    </li>
+                )
+            });
+
+        return (
+            <Auxiliary>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Auxiliary>
+        );
+    }
+}
+
+export default OrderSummaru;
