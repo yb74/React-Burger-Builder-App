2f6bfd550c78cf58ba628d11a235559a664ca1d4
younes <yb74@hotmail.fr>
2020-11-17 23:32:43 +0100

    Setting up a JS Config for the Form. Dynamically Create Inputs based on JS Config. Adding a Dropdown Component. Handling User Input. Handling Form Submission.

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -2,18 +2,44 @@ import React from 'react';
 
 import classes from './Input.module.css'
 
-const input = props => {
+const input = ( props ) => {
     let inputElement = null;
 
-    switch (props.inputtype) {
-        case ('input'):
-            inputElement = <input className={classes.InputElement} {...props}/>
+    switch ( props.elementType ) {
+        case ( 'input' ):
+            inputElement = <input
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
             break;
-        case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props}/>
+        case ( 'textarea' ):
+            inputElement = <textarea
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+            break;
+        case ( 'select' ):
+            inputElement = (
+                <select
+                    className={classes.InputElement}
+                    value={props.value}
+                    onChange={props.changed}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props}/>
+            inputElement = <input
+                className={classes.InputElement}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
     }
 
     return (
@@ -22,6 +48,7 @@ const input = props => {
             {inputElement}
         </div>
     );
+
 };
 
 export default input;
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Route} from "react-router-dom";
 
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
-import ContactData from "../../components/ContactData/ContactData";
+import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
     state = {
