1f744f13502f4e18001ee1c2e364bab7f3ffee45
younes <yb74@hotmail.fr>
2021-01-10 14:45:26 +0100

    Testing Implementing some unit tests using Jest and Enzyme API (testing components and redux)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,7 +11,7 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
 import axios from '../../axios-orders';
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component { // class exported here without access to the store (for testing purpose)
 
     state = {
         purchasing: false
